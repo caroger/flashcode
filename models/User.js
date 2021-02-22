275c14389c5c3a0da10add820258bdbbc76e42b5
Chubbibanana <dongsoo.tchah@gmail.com>
2021-02-22 12:05:10 -0800

    Modify User model to include cards assc

--- models/User.js
+++ models/User.js
@@ -14,7 +14,8 @@ const UserSchema = new Schema(
     password: {
       type: String,
       required: true
-    }
+    },
+    cards: [{ type: Schema.Types.ObjectId, ref: 'Card'}]
   },
   {
     timestamps: true
