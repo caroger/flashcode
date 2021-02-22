37570cbbe28ea773f7afe467c36a43093f4c46a3
colineckert <colineckert10@gmail.com>
2021-02-22 11:00:40 -0800

    Create card index container with fetchCards action template

--- frontend/src/components/cards/card_index.js
+++
@@ -1,3 +0,0 @@
-// fetchDeckCards given selected deck
-// if no deck, show cards to review today
-// create card and "feeling lucky" buttons
---
+++ frontend/src/components/cards/cards_index.js
@@ -0,0 +1,15 @@
+// fetchDeckCards given selected deck
+// if no deck, show cards to review today
+// create card and "feeling lucky" buttons
+
+import React, { Component } from 'react'
+
+export default class CardsIndex extends Component {
+  render() {
+    return (
+      <div>
+        Cards
+      </div>
+    )
+  }
+}
