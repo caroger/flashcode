4e51c9c622eeab5ba1f9ae33f10f2bc68afe4b46
colineckert <colineckert10@gmail.com>
2021-02-22 14:09:09 -0800

    Add link to card in cards list render

--- frontend/src/components/cards/cards_list.js
+++ frontend/src/components/cards/cards_list.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Card from './card';
 
 export default class CardsList extends Component {
   constructor(props) {
@@ -11,14 +10,16 @@ export default class CardsList extends Component {
     // pull out cards and deck to render list of cards depending on view/route
     const { cards, deck } = this.props;
 
-    // update cardUrl if view is deck
+    let cardUrl = "/cards/";
+    // update cardUrl if view is "deck"
 
     const cardList = cards.map(card => {
       if (!card) return null;
-
       return (
         <div>
-          
+          <Link to={`${cardUrl}${card.id}`} >
+
+          </Link>
         </div>
       )
     })
