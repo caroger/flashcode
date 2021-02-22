9033fe1f16d9d55b1df6dd0edface2e7798271f1
Edwin <edwinzhou99@gmail.com>
2021-02-22 11:46:56 -0800

    add leftnav and feed render

--- frontend/src/components/left_nav/left_nav.js
+++ frontend/src/components/left_nav/left_nav.js
@@ -1,2 +1,24 @@
 // render logo, user handle, search (to be implemented later)
 // 
+import React from 'react';
+import DecksIndex from './decks/decks_index';
+
+class LeftNav extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        
+        return(
+            <div className='left-nav-div'>
+                <h1>Logo goes here</h1>
+                <div>Welcome, {this.props.currentUser}</div>
+                <div>Index link goes here</div>
+                
+            </div>
+        )
+    }
+
+}
+
+export default LeftNav;
--- frontend/src/components/right_nav/feed.js
+++ frontend/src/components/right_nav/feed.js
@@ -1,5 +1,38 @@
-// render achievements feed for last 24hrs
-// achievement - array of objects to be rendered on login
-// use selector to grab achievements array, slicing for date/time
-// need to figure out milestones for updates
-// do we need another table?
+// - Render achievements feed for last 24hrs
+// - Achievement - array of objects to be rendered on login
+// - Use selector to grab achievements array, slicing for date/time
+// - Need to figure out milestones for updates
+// - Do we need another table?
+//--------------------------------------------------------------------------------------------------
+
+import React from 'react';
+
+class Feed extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    //add a helper function here that iterates thru array of achievements to load onto feed.
+    //24 hour max
+    
+
+    render(){
+        //css overflow scroll
+        return(
+            <div className='feed-div'>
+                <h4 className='feed-title'>News Feed</h4>
+                {/* feed title letter spacing */}
+                <ul className ='feed-ul'>
+                    <li className='feed-li'>Dongsoo has completed his first problem!</li>
+                    <li className='feed-li'>Dongsoo has completed 5 problems in a row!</li>
+                    <li className='feed-li'> Dongsoo has completed 10 problems in a row!</li>
+                    <li className='feed-li'>Dongsoo has completed 20 problems in a row!</li>
+                    <li className='feed-li'>Dongsoo has completed 30 problems in a row!</li>
+                    <li className='feed-li'>Dongsoo has completed 40 problems in a row!</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Feed;
