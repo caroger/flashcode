b44d0ccf1a40dbf213de1a95b0ee329bde8ded07
caroger <rogerhu1989@gmail.com>
2021-02-23 15:35:41 -0800

    refector_lcAPI

--- routes/api/lc_api.js
+++ routes/api/lc_api.js
@@ -4,9 +4,9 @@ axios.get('https://leetcode.com/api/problems/all/').then((res) => {
   lcdata = res.data;
 });
 function findProblem(probNum, input = lcdata) {
-  return input['stat_status_pairs']
-    .map((stat) => stat['stat'])
-    .find((stat) => stat['question_id'] === probNum);
+  problem = input['stat_status_pairs'].filter((pair) => pair.stat.question_id === probNum)[0];
+
+  return problem;
 }
 
 module.exports = findProblem;
