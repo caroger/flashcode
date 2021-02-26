be5a61643d710fd11424366bf7e64da247bf0cdd
caroger <rogerhu1989@gmail.com>
2021-02-26 00:28:26 -0800

    finished right nav styling

--- frontend/src/components/right_nav/feed.js
+++ frontend/src/components/right_nav/feed.js
@@ -6,7 +6,9 @@
 //--------------------------------------------------------------------------------------------------
 
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import * as FA from '@fortawesome/free-brands-svg-icons';
 class Feed extends React.Component {
   constructor(props) {
     super(props);
@@ -15,25 +17,36 @@ class Feed extends React.Component {
   //add a helper function here that iterates thru array of achievements to load onto feed.
   //24 hour max
 
+  renderfeed(username, num, icon) {
+    return (
+      <div className="feed-item">
+        <h4>
+          <FontAwesomeIcon icon={icon} /> {`${username}`}
+        </h4>
+        <br></br>
+        {`Completed problem number ${num} on LeetCode!`}
+      </div>
+    );
+  }
+
   render() {
     //css overflow scroll
     return (
       <div className="feed-container">
-        <div className="feed-header">News Feed</div>
+        <div className="feed-header">
+          <h1>
+            <FontAwesomeIcon icon={faUsers} /> User Feeds
+          </h1>
+        </div>
         <div className="feed-body-container">
-          <div className="feed-item">Dongsoo has completed 5 problems in a row!</div>
-          <div className="feed-item"> Dongsoo has completed 10 problems in a row!</div>
-          <div className="feed-item">Dongsoo has completed 20 problems in a row!</div>
-          <div className="feed-item">Dongsoo has completed 30 problems in a row!</div>
-          <div className="feed-item">Dongsoo has completed 40 problems in a row!</div>
-          <div className="feed-item">Dongsoo has completed 50 problems in a row!</div>
-          <div className="feed-item">Dongsoo has completed 60 problems in a row!</div>
-          <div className="feed-item">Dongsoo has completed 70 problems in a row!</div>
-          <div className="feed-item">Dongsoo has completed 80 problems in a row!</div>
-          <div className="feed-item">Dongsoo has completed 90 problems in a row!</div>
-          <div className="feed-item">Dongsoo has completed 100 problems in a row!</div>
-          <div className="feed-item">Dongsoo has completed 100 problems in a row!</div>
-          <div className="feed-item">Dongsoo has completed 100 problems in a row!</div>
+          {this.renderfeed('Dongsoo', '7', FA.faJava)}
+          {this.renderfeed('Colin', '101', FA.faPython)}
+          {this.renderfeed('Edwin', '77', FA.faNodeJs)}
+          {this.renderfeed('Roger', '79', FA.faPhp)}
+          {this.renderfeed('Ryan', '979', FA.faNode)}
+          {this.renderfeed('Erik', '159', FA.faReact)}
+          {this.renderfeed('Julia', '555', FA.faCss3)}
+          {this.renderfeed('Lina', '777', FA.faReact)}
         </div>
       </div>
     );
