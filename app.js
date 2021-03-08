ba39ae365c91ee475957d2c343f9e4ce37377369
Chubbibanana <dongsoo.tchah@gmail.com>
2021-03-07 22:20:59 -0800

    Encounter bug with updating cards

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const cards = require("./routes/api/cards");
 const decks = require('./routes/api/decks');
+const messages = require('./routes/api/messages');
 const passport = require('passport');
 const app = express();
 const path = require('path');
@@ -27,6 +28,7 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/cards', cards);
 app.use('/api/decks', decks);
+app.use('/api/messages', messages);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
