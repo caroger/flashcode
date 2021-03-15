aecd5aecb8be27816d14062e0d2a90b2d6ea5fad
colineckert <colineckert10@gmail.com>
2021-03-15 12:21:50 -0700

    Add Redirect to app.js routes

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
 import Modal from './modal/modal';
@@ -13,11 +13,15 @@ const App = () => (
     <Route component={NavBarContainer} />
     <Route component={Modal} />
 
-    <ProtectedRoute path="/home" component={MainPage} />
-    <ProtectedRoute path="/decks/:deckId" component={MainPage} />
+    <Switch>
+      <ProtectedRoute path="/home" component={MainPage} />
+      <ProtectedRoute path="/decks/:deckId" component={MainPage} />
 
-    <Route exact path="/team" component={TeamPage} />
-    <AuthRoute exact path="/" component={LoggedOutComponent} />
+      <Route exact path="/team" component={TeamPage} />
+      <AuthRoute exact path="/" component={LoggedOutComponent} />
+
+      <Route render={() => <Redirect to={{pathname: "/"}} />} />
+    </Switch>
   </div>
 );
 
--- frontend/src/components/session/signup_form_container.js
+++ frontend/src/components/session/signup_form_container.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { signup, clearErrors } from '../../actions/session_actions';
+import { signup, clearErrors, login } from '../../actions/session_actions';
 import SessionForm from './session_form';
 import { closeModal } from '../../actions/modal_actions';
 
@@ -13,6 +13,7 @@ const mapStateToProps = ({ errors }) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     processForm: (user) => dispatch(signup(user)),
+    login: (user) => dispatch(login(user)),
     closeModal: () => dispatch(closeModal()),
     clearErrors: () => dispatch(clearErrors())
   };
