0b140332e2dbc6fcd61418a5939eec88409a9bb5
Chubbibanana <dongsoo.tchah@gmail.com>
2021-03-16 15:35:04 -0700

    test with join instead of resolve

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const path = require('path');
   app.use(express.static('frontend/build'));
 
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
 // }
 
