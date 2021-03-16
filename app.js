f781b2da8a1469f39140ee4a8fe091d849843894
Chubbibanana <dongsoo.tchah@gmail.com>
2021-03-16 11:03:51 -0700

    Fix app crash error

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ const app = express();
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
-  app.get('/*', (req, res) => {
-    res.redirect('/');
-  })
+
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
