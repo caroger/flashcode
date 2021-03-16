8da281e1d85199d7c77e66b81e0d2ca7fb85253f
Chubbibanana <dongsoo.tchah@gmail.com>
2021-03-15 20:32:39 -0700

    Add handling of invalid get requests

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = express();
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
+  app.get('/*', (req, res) => {
+    res.redirect('/');
+  })
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
