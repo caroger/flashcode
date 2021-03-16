dbbaa85c2cc8f9741848e713e8c7411eaf91e93e
Chubbibanana <dongsoo.tchah@gmail.com>
2021-03-16 15:58:11 -0700

    Fixing cannot get error

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.use('/api/users', users);
 app.use('/api/cards', cards);
 app.use('/api/decks', decks);
 app.use('/api/messages', messages);
+app.get('*', function (req, res) {
+  res.redirect('/');
+});
 const port = process.env.PORT || 5000;
 
 // app.listen(port, () => console.log(`Server is running on port ${port}`));
