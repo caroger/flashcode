8085d73e80bb47b9554029486f30cc1d29a1229d
Chubbibanana <dongsoo.tchah@gmail.com>
2021-03-16 15:42:32 -0700

    Test for heroku

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const path = require('path');
 // if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
 
-  app.get('/', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
 // }
